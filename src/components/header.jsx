df5c97540fd3f62e4379cd10d16ef57de3d9eba2
hasnain-mawia <110049696+hasnain-mawia@users.noreply.github.com>
2024-03-29 11:03:35 +0500

    logo text change

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,7 +13,7 @@ function Header() {
   return (
     <header className='bg-white text-black dark:bg-[#2b3945] dark:text-white h-[80px] shadow-lg shadow-black/20 sticky top-0'>
         <div className="max-w-[1600px] xs:px-3 h-full mx-auto flex justify-between items-center">
-           <h1 className='text-[26px] xs:text-[22px] font-semibold'><a href="/">Where in the worlds?</a></h1> 
+           <h1 className='text-[26px] xs:text-[20px] font-semibold'><a href="/">Where in the worlds?</a></h1> 
            <div className='flex items-center gap-3'>  
            <div onClick={toggleDarkMode} className="'bg-white text-black dark:bg-[#fff] dark:text-[#2b3945] flex items-center gap-2 border-[1px] border-[#1b1b1b] p-2 rounded-3xl cursor-pointer">
            {darkmode ? <FaSun/>: <FaRegMoon/>} 
